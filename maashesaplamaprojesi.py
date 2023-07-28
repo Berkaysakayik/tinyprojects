db353f9415da61e3a6706abca0bae117bc47ba9e
Berkay <drops3305@gmail.com>
2023-07-29 00:14:23 +0300

    Create maashesaplamaprojesi.py

---
+++ maashesaplamaprojesi.py
@@ -0,0 +1,9 @@
+sabitmaas = 12500
+deneyimartisi = 100
+cocukbasina = 250
+mesaisaatlik = 50
+
+
+sorucevap = deneyimartisi*5 + cocukbasina*2 + mesaisaatlik*20 + sabitmaas
+
+print(f"Alacağı maaş {sorucevap} TL" )
