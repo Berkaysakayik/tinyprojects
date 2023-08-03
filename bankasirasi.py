3605fcf5c157684b9503b98326c413bce06cac58
Berkay <drops3305@gmail.com>
2023-08-03 15:25:29 +0300

    Create bankasirasi.py

---
+++ bankasirasi.py
@@ -0,0 +1,19 @@
+ad = input("İsminizi giriniz: ")
+soyad = input("Soyisminizi giriniz: ")
+sirano = input("Sıra numaranızı giriniz: ")
+beklemesuresi = input("Bekleme sürenizi giriniz: ")
+
+print(f"Merhaba {ad} {soyad}, {sirano} numaralı sıradasınız, yaklaşık {beklemesuresi} dakika sonra görüşmeye katılacaksınız.")
+
+print("Merhaba {} {}, {} numaralı sıradasınız, yaklaşık {} dakika sonra görüşmeye katılacaksınız.".format(ad,soyad,sirano,beklemesuresi))
+
+print("Merhaba "+ad +" "+soyad +", "+sirano + " numaralı sıradasınız, yaklaşık " +beklemesuresi+ " dakika sonra görüşmeye katılacaksınız.")
+
+print("Merhaba",ad , soyad ,",", sirano, "numaralı sıradasınız, yaklaşık",beklemesuresi, "dakika sonra görüşmeye katılacaksınız.")
+
+
+
+
+
+
+
